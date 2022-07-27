a16588fe447ef20609fbcd84e9c1cb7bf2b72ddd
Bevis Lin <bevis.lin@icloud.com>
2022-07-27 16:33:11 +0800

    buy listing works

--- pages/listings/[id].js
+++ pages/listings/[id].js
@@ -1,11 +1,17 @@
+import { useEffect, useState } from 'react';
 import {
   getOpenListingByListingId,
   getOpenListingIds,
-} from "../../util/interact";
-var Web3 = require("web3");
+} from '../../util/interact';
+const marketplaceContractABI = require('../../abi/marketplace-abi.json');
+const marketplaceContractAddress =
+  process.env.NEXT_PUBLIC_MARTKETPLACE_CONTRACT;
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+const web3 = createAlchemyWeb3(alchemyKey);
 
 export const getStaticPaths = async () => {
-  console.log("getting sale listings..");
+  console.log('getting sale listings..');
   let openListingIds = await getOpenListingIds();
   const paths = openListingIds.map((listingId) => {
     return {
@@ -21,7 +27,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  console.log("getStaticProps been called....,id:", id);
+  console.log('getStaticProps been called....,id:', id);
   let listing = await getOpenListingByListingId(id);
   listing = JSON.stringify(listing);
   return {
@@ -30,16 +36,108 @@ export const getStaticProps = async (context) => {
 };
 
 const Details = ({ listing }) => {
+  const [install, setInstall] = useState(false);
+  const [network, setNetwork] = useState(false); //False if wrong network
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    const checkMetamask = async () => {
+      setInstall(!window.ethereum);
+      if (window.ethereum) {
+        const networkVersion = await window.ethereum.request({
+          method: 'net_version',
+        });
+        console.log(networkVersion);
+        setNetwork(networkVersion == process.env.NEXT_PUBLIC_NET_VERSION);
+
+        window.ethereum.on('chainChanged', (networkVersion) => {
+          console.log('chainChanged', networkVersion);
+          //window.location.reload();
+          let nv10 = parseInt(networkVersion);
+          setNetwork(nv10 == process.env.NEXT_PUBLIC_NET_VERSION);
+        });
+      }
+    };
+
+    checkMetamask();
+  }, []);
+
+  if (install) return <h2>Install MetaMask</h2>;
+  if (!network) return <h2>Wrong Network</h2>;
+
+  const onPurchasePressed = async () => {
+    var listingVar = JSON.parse(listing);
+    console.log(listingVar.id);
+    console.log(listingVar.price);
+    let callResult = await purchaseListing(listingVar.id, listingVar.price);
+
+    if (callResult.success) {
+      console.log('purchase transaction sent...');
+    } else {
+      console.log(`purchase transaction failed, error:${callResult.status}`);
+    }
+
+    setStatus(callResult.status);
+  };
+
+  const purchaseListing = async (listingId, price) => {
+    window.contract = await new web3.eth.Contract(
+      marketplaceContractABI,
+      marketplaceContractAddress
+    );
+
+    console.log(window.contract);
+
+    //console.log(listingId, price);
+
+    console.log(listingId);
+    console.log(price);
+
+    const valueToSend = web3.utils.toHex(price);
+
+    const transactionParameters = {
+      to: marketplaceContractAddress, // Required except during contract publications.
+      from: window.ethereum.selectedAddress, // must match user's active address.
+      value: valueToSend,
+      data: window.contract.methods
+        .purchase(listingId, window.ethereum.selectedAddress)
+        .encodeABI(),
+    };
+
+    try {
+      const txHash = await window.ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+      });
+      return {
+        success: true,
+        status:
+          '✅ Check out your transaction on Etherscan: https://mumbai.polygonscan.com/tx/' +
+          txHash,
+        data: txHash,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: '😥 Something went wrong: ' + error.message,
+      };
+    }
+  };
+
   //return <div>OK</div>;
   console.log(listing);
   var listingVar = JSON.parse(listing);
   return (
     <div>
       <h1>Listing Id:{listingVar.id}</h1>
-      <h1>Price: {Web3.utils.fromWei(listingVar.price, "ether")} ether</h1>
+      <h1>Price: {web3.utils.fromWei(listingVar.price, 'ether')} ether</h1>
       <h1>{listingVar.emperor.name}</h1>
       <img src={listingVar.emperor.imageUrl} />
       <h3>{listingVar.emperor.description}</h3>
+      <button onClick={onPurchasePressed}>Buy</button>
+      <p id="status" style={{ color: 'red' }}>
+        {status}
+      </p>
     </div>
   );
   //return <div>OK</div>;
