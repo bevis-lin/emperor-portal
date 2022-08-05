e33d709e0570959e3f06c569af786605b328e6fe
kunyilin <bevis.lin@icloud.com>
2022-08-05 16:16:22 +0800

    sign message in profile page

--- pages/member/profile.js
+++ pages/member/profile.js
@@ -1,54 +1,59 @@
-import { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 
 const Profile = () => {
   const [install, setInstall] = useState(false);
   const [network, setNetwork] = useState(false); //False if wrong network
-  const [status, setStatus] = useState('');
+  const [status, setStatus] = useState("");
   const [signature, setSignature] = useState();
-  const [cookies, setCookie] = useCookies(['signature']);
+  const [cookies, setCookie] = useCookies(["signature"]);
 
-  const verifyMessage = async (messageToVerify, signature) => {
+  const signMessage = async (messageToSign) => {
     try {
       const from = window.ethereum.selectedAddress;
-      const recoveredAddr = web3.eth.accounts.recover(
-        messageToVerify,
-        signature
-      );
-      if (from.toLowerCase() == recoveredAddr.toLowerCase()) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (err) {
-      console.log(err);
-      return false;
+      const sign = await window.ethereum.request({
+        method: "personal_sign",
+        params: [messageToSign, from, "emperor"],
+      });
+
+      console.log("sign : " + sign);
+
+      return {
+        success: true,
+        status: "Sign successfully",
+        data: sign,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: "😥 Something went wrong: " + error.message,
+      };
     }
   };
 
   const onSignMessagePressed = async () => {
     const message =
-      'Welcome to vist Emperor, this request is to get a signature from you, here after we will use this signature to get your wallet address';
+      "Welcome to vist Emperor, this request is to get a signature from you, here after we will use this signature to get your wallet address";
     const signResult = await signMessage(message);
     if (signResult.success) {
       setSignature(signResult.data);
       //cookie.set('signature', true, signResult.data);
-      setCookie('signature', signResult.data, { path: '/' });
+      setCookie("signature", signResult.data, { path: "/" });
     }
   };
 
-  useEffect(async () => {
+  useEffect(() => {
     const checkMetamask = async () => {
       setInstall(!window.ethereum);
       if (window.ethereum) {
         const networkVersion = await window.ethereum.request({
-          method: 'net_version',
+          method: "net_version",
         });
         console.log(networkVersion);
         setNetwork(networkVersion == process.env.NEXT_PUBLIC_NET_VERSION);
 
-        window.ethereum.on('chainChanged', (networkVersion) => {
-          console.log('chainChanged', networkVersion);
+        window.ethereum.on("chainChanged", (networkVersion) => {
+          console.log("chainChanged", networkVersion);
           //window.location.reload();
           let nv10 = parseInt(networkVersion);
           setNetwork(nv10 == process.env.NEXT_PUBLIC_NET_VERSION);
@@ -56,27 +61,56 @@ const Profile = () => {
       }
     };
 
+    const verifyMessage = async (messageToVerify, signature) => {
+      try {
+        const from = window.ethereum.selectedAddress;
+        const recoveredAddr = web3.eth.accounts.recover(
+          messageToVerify,
+          signature
+        );
+        if (from.toLowerCase() == recoveredAddr.toLowerCase()) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    };
+
     checkMetamask();
 
     if (network) {
-      const signatureFromCookie = cookies.signature;
-      if (!signatureFromCookie) {
-        return;
-      }
-      console.log(signatureFromCookie);
+      setStatus("checking signature......");
+      console.log("checking signature.....");
+      if (cookies) {
+        const signatureFromCookie = cookies.signature;
+        if (!signatureFromCookie) {
+          setStatus("This address has no signature.");
+        } else {
+          console.log(signatureFromCookie);
 
-      let checkSignature = await verifyMessage(
-        'Welcome to vist Emperor, this request is to get a signature from you, here after we will use this signature to get your wallet address',
-        signatureFromCookie
-      );
+          let checkSignature = verifyMessage(
+            "Welcome to vist Emperor, this request is to get a signature from you, here after we will use this signature to get your wallet address",
+            signatureFromCookie
+          );
 
-      if (checkSignature) {
-        setStatus('This address has signature.');
+          if (checkSignature) {
+            setStatus("This address has signature.");
+            setSignature(cookies.signature);
+          } else {
+            setStatus("This address has no signature.");
+          }
+        }
       } else {
-        setStatus('This address has no signature.');
+        setStatus("no singnature cookie found.");
       }
     }
-  }, [walletAddress]);
+  }, [network, signature]);
+
+  if (install) return <p>Install MetaMask</p>;
+  if (!network) return <p>Detecting network or wrong network detected.</p>;
 
   return (
     <div>
@@ -91,7 +125,7 @@ const Profile = () => {
           Sign
         </button>
       )}
-      <p id="status" style={{ color: 'red' }}>
+      <p id="status" style={{ color: "red" }}>
         {status}
       </p>
     </div>
