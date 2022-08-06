d4b76eecf66794e1463b59162dd89152ec679636
Bevis Lin <bevis.lin@icloud.com>
2022-08-06 15:04:08 +0800

    sign message page

---
+++ pages/member/sign.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+
+const Sign = () => {
+  const [install, setInstall] = useState(false);
+  const [network, setNetwork] = useState(false); //False if wrong network
+  const [cookies, setCookie] = useCookies(['signature']);
+
+  const signMessage = async (messageToSign) => {
+    try {
+      const from = window.ethereum.selectedAddress;
+      const sign = await window.ethereum.request({
+        method: 'personal_sign',
+        params: [messageToSign, from, 'emperor'],
+      });
+
+      console.log('sign : ' + sign);
+
+      return {
+        success: true,
+        status: 'Sign successfully',
+        data: sign,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: '😥 Something went wrong: ' + error.message,
+      };
+    }
+  };
+
+  const onSignMessagePressed = async () => {
+    const message =
+      'Welcome to vist Emperor, this request is to get a signature from you, here after we will use this signature to get your wallet address';
+    const signResult = await signMessage(message);
+    if (signResult.success) {
+      //cookie.set('signature', true, signResult.data);
+      setCookie('signature', signResult.data, { path: '/' });
+    }
+  };
+
+  useEffect(() => {
+    const checkMetamask = async () => {
+      setInstall(!window.ethereum);
+      if (window.ethereum) {
+        const networkVersion = await window.ethereum.request({
+          method: 'net_version',
+        });
+        console.log(networkVersion);
+        setNetwork(networkVersion == process.env.NEXT_PUBLIC_NET_VERSION);
+
+        window.ethereum.on('chainChanged', (networkVersion) => {
+          console.log('chainChanged', networkVersion);
+          //window.location.reload();
+          let nv10 = parseInt(networkVersion);
+          setNetwork(nv10 == process.env.NEXT_PUBLIC_NET_VERSION);
+        });
+      }
+    };
+
+    checkMetamask();
+  }, []);
+
+  if (install) return <p>Install MetaMask</p>;
+  if (!network) return <p>Detecting network or wrong network detected.</p>;
+
+  return (
+    <div>
+      <h1>Sign Message</h1>
+      <br />
+      {network && (
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={onSignMessagePressed}
+        >
+          Sign
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Sign;
